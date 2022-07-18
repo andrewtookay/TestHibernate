1b3e4d8b322f4a91cf4fa75ed04d159faed5dbd1
andrewtookay <andrewtookay@gmail.com>
2022-07-18 21:00:01 +0300

    test hibernate setup with dockerized MySQL

---
+++ src/main/java/BookManager.java
@@ -0,0 +1,48 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class BookManager {
+    protected SessionFactory sessionFactory;
+
+    protected void setup() {
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+    protected void exit() {
+        // code to close Hibernate Session factory
+    }
+
+    protected void create() {
+        Book book = new Book();
+        book.setTitle("Effective Java");
+        book.setAuthor("Joshua Bloch");
+        book.setPrice(32.59f);
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.persist(book);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    protected void read() {
+        // code to get a book
+    }
+
+    protected void update() {
+        // code to modify a book
+    }
+
+    protected void delete() {
+        // code to remove a book
+    }
+
+    public static void main(String[] args) {
+        BookManager manager = new BookManager();
+        manager.setup();
+        manager.create();
+        manager.exit();
+    }
+}
